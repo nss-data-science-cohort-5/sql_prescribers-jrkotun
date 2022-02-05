96730d532de678865d8f3afe39285ccdabea4ec1
jrkotun <hjrkotun@gmail.com>
2022-02-05 14:39:28 -0600

    Finished Part 2

--- Habeeb_SQL_Prescribers.sql
+++ Habeeb_SQL_Prescribers.sql
@@ -3,6 +3,7 @@
 -- February 1, 2022
 -- SQL Prescribers
 
+--- Part 1
 -- Question 1a
 SELECT npi, SUM(total_claim_count)
 FROM prescription
@@ -14,8 +15,8 @@ LIMIT 1;
 -- Question 1b
 SELECT p1.nppes_provider_first_name AS first_name, 
 	   p1.nppes_provider_last_org_name AS last_name,
-	   p1.specialty_description,
-	   SUM(p2.total_claim_count)
+	   p1.specialty_description AS specialty,
+	   SUM(p2.total_claim_count) AS total_claims
 FROM prescriber AS p1
 INNER JOIN prescription AS p2
 ON p1.npi = p2.npi
@@ -26,18 +27,18 @@ LIMIT 1;
 
 -- Question 2a
 SELECT p1.specialty_description, 
-	   SUM(p2.total_claim_count) AS total_specialty_claims
+	   SUM(p2.total_claim_count) AS total_claims
 FROM prescriber AS p1
 INNER JOIN prescription AS p2
 ON p1.npi = p2.npi
 GROUP BY p1.specialty_description
-ORDER BY total_specialty_claims DESC
+ORDER BY total_claims DESC
 LIMIT 1;
 -- Answer: Family Practice is the specialty with the most total number of claims.
 
 -- Question 2b
 SELECT p1.specialty_description, 
-	   SUM(p2.total_claim_count) AS total_specialty_claims
+	   SUM(p2.total_claim_count) AS total_claims
 FROM prescriber AS p1
 INNER JOIN prescription AS p2
 ON p1.npi = p2.npi
@@ -45,7 +46,7 @@ INNER JOIN drug AS d
 ON p2.drug_name = d.drug_name
 WHERE d.opioid_drug_flag = 'Y'
 GROUP BY p1.specialty_description
-ORDER BY total_specialty_claims DESC
+ORDER BY total_claims DESC
 LIMIT 1;
 -- Answer: Nurse Practitioner is the specialty with the most total number of claims for opioids.
 
@@ -78,7 +79,8 @@ FROM prescription AS p
 INNER JOIN drug AS d
 ON p.drug_name = d.drug_name
 GROUP BY d.generic_name
-ORDER BY 3 DESC;
+ORDER BY 3 DESC
+LIMIT 1;
 -- Answer: C1 Esterase is the drug with the hightest total cost per day.
 
 -- Question 4a
@@ -185,3 +187,177 @@ FROM(
 LEFT JOIN prescription
 USING (npi, drug_name)
 ORDER BY npi, drug_name, total_claim_count DESC;
+
+--- Part 2
+-- Question 1
+SELECT (SELECT COUNT(DISTINCT npi) AS npi_count
+		FROM prescriber) - 
+		(SELECT COUNT(DISTINCT npi) AS npi_count
+		FROM prescription) AS npi_difference
+/* Answer: There are 4458 NPIs that appear in the prescriber table but not 
+in the prescription table.*/
+
+-- Question 2a
+SELECT drug.generic_name, SUM(total_claim_count) AS total_count
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+INNER JOIN drug
+USING(drug_name)
+WHERE specialty_description = 'Family Practice'
+GROUP BY generic_name
+ORDER BY total_count DESC
+LIMIT 5;
+-- Answer: See genereated table.
+
+-- Question 2b
+SELECT drug.generic_name, SUM(total_claim_count) AS total_count
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+INNER JOIN drug
+USING(drug_name)
+WHERE specialty_description = 'Cardiology'
+GROUP BY generic_name
+ORDER BY total_count DESC
+LIMIT 5;
+-- Answer: See genereated table.
+
+-- Question 2c
+WITH family_practice AS
+	(SELECT generic_name, SUM(total_claim_count) AS total_count
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 INNER JOIN drug
+	 USING(drug_name)
+	 WHERE specialty_description = 'Family Practice'
+	 GROUP BY generic_name
+	 ORDER BY total_count DESC
+	 LIMIT 5),
+cardiology AS 
+	(SELECT generic_name, SUM(total_claim_count) AS total_count
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 INNER JOIN drug
+	 USING(drug_name)
+	 WHERE specialty_description = 'Cardiology'
+	 GROUP BY generic_name
+	 ORDER BY total_count DESC
+	 LIMIT 5)
+SELECT generic_name
+FROM family_practice
+INTERSECT
+SELECT generic_name
+FROM cardiology;
+-- Answer: Atorvastatin Calcium and Amplodipine Besylate are contained in both queries.
+
+-- Question 3a
+SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'NASHVILLE'
+GROUP BY npi, nppes_provider_city
+ORDER BY total_claims DESC
+LIMIT 5;
+
+-- Question 3b
+SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+FROM prescriber
+INNER JOIN prescription
+USING (npi)
+WHERE nppes_provider_city = 'MEMPHIS'
+GROUP BY npi, nppes_provider_city
+LIMIT 5;
+
+-- Question 3c
+WITH nashville AS
+	(SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 WHERE nppes_provider_city = 'NASHVILLE'
+	 GROUP BY npi, nppes_provider_city
+	 LIMIT 5),
+memphis AS 
+	(SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 WHERE nppes_provider_city = 'MEMPHIS'
+	 GROUP BY npi, nppes_provider_city
+	 LIMIT 5),
+knoxville AS 
+	(SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 WHERE nppes_provider_city = 'KNOXVILLE'
+	 GROUP BY npi, nppes_provider_city
+	 LIMIT 5),
+chattanooga AS 
+	(SELECT DISTINCT npi, 
+	   nppes_provider_city AS city, 
+	   SUM(total_claim_count) AS total_claims
+	 FROM prescriber
+	 INNER JOIN prescription
+	 USING (npi)
+	 WHERE nppes_provider_city = 'CHATTANOOGA'
+	 GROUP BY npi, nppes_provider_city
+	 LIMIT 5)
+SELECT *
+FROM nashville
+UNION
+SELECT *
+FROM memphis
+UNION
+SELECT *
+FROM knoxville
+UNION
+SELECT *
+FROM chattanooga
+ORDER BY city, total_claims DESC;
+
+-- Question 4
+SELECT county, 
+	   overdose_deaths
+FROM fips_county
+INNER JOIN overdose_deaths
+USING (fipscounty)
+WHERE year = 2017
+AND overdose_deaths > 
+	(SELECT AVG(overdose_deaths)
+	 FROM overdose_deaths
+	 WHERE year = 2017)
+ORDER BY overdose_deaths DESC;
+
+-- Question 5a
+SELECT SUM(population) as total_tennessee_population
+FROM population;
+-- The total population of Tennessee is 6,597,381.
+
+-- Question 5b
+SELECT county, 
+	   population,
+	   ROUND(population *100 / 
+	   	(SELECT SUM(population)
+		 FROM population), 2) AS population_percentage
+FROM fips_county
+INNER JOIN population
+USING (fipscounty)
+ORDER BY population_percentage DESC;
+
+
+
